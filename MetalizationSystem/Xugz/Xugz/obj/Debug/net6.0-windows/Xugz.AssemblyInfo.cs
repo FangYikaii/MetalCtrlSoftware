8f6852e318e902e2bdfeed8091034cd69433c516
fyk <342363948@qq.com>
2025-12-15 10:50:05 +0800

    add db
    add SampleDataDB

--- MetalizationSystem/Xugz/Xugz/obj/Debug/net6.0-windows/Xugz.AssemblyInfo.cs
+++ MetalizationSystem/Xugz/Xugz/obj/Debug/net6.0-windows/Xugz.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Xugz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03254b2207645f44b00300d8439999ef48687dd5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Xugz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Xugz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
